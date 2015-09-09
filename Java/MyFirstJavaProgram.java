b92a64b631081ec02c400539d50aac95fd90f4b5
nick <chengang3368@sina.com>
2015-09-09 20:25:58 +0800

    kkk

--- Java/MyFirstJavaProgram.java
+++ Java/MyFirstJavaProgram.java
@@ -2,5 +2,6 @@ public class MyFirstJavaProgram {
 	public static void main(String []args) {
 		System.out.println("Hello World");
 		System.out.println("hei boy");
+		System.out.println("kkkkk");
 	}
 }
